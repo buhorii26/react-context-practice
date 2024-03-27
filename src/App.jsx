3c88257582bd54942904cbbfe018855c00dac42f
Buhori <buhorics@gmail.com>
2024-03-27 10:15:47 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ToggleTheme from "./components/ToggleTheme";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: localStorage.getItem("theme") || "light",
+      toggleTheme: () => {
+        this.setState((prevState) => {
+          // mendapatkan nilai tema baru berdasarkan state sebelumnya
+          const newTheme = prevState.theme === "light" ? "dark" : "light";
+          // menyimpan nilai tema baru ke local storage
+          localStorage.setItem("theme", newTheme);
+          return {
+            theme: newTheme,
+          };
+        });
+      },
+    };
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.theme !== this.state.theme) {
+      document.documentElement.setAttribute("data-theme", this.state.theme);
+    }
+  }
+
+  componentDidMount() {
+    document.documentElement.setAttribute("data-theme", this.state.theme);
+  }
+
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <ToggleTheme />
+          <p>
+            Hello, {this.state.theme === "light" ? "Early Bird" : "Night Owl"}.
+          </p>
+        </div>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
